62cd8a3eea6056c95baded1768dae8b5fcb0622b
M4RT1N <mjlynch35@yahoo.com>
2023-02-22 18:55:28 -0600

    Added the ability to load and save local storage

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 var today = dayjs();
 var save = $("button");
+var currentHour = today.format("H");
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -13,15 +11,38 @@ $(function () {
   // useful when saving the description in local storage?
 
   // This is the event handler for the save button
-  save.on("click", function() {
-    alert("Hello");
+  // .click does the same thing as .on("click");
+  save.click(function() {
+    // Get the id of the previous sibling with the class description and then we get that id of that element
+    var inputId = $(this).prev('.description').attr('id');
+    //Get the value of the id we got in the previous line
+    var task = $(this).prev('.description').val();
+    // Get any items saved to local storage, if tasks is empty then we will create a new object
+    var taskObj = JSON.parse(localStorage.getItem('tasks')) || {};
+
+    // Save the input value task to the correct id in taskObj
+    taskObj[inputId] = task;
+
+    // Save taskObj to local storage
+    localStorage.setItem('tasks', JSON.stringify(taskObj));
   });
 
-  var currentHour = today.format("H");
-  console.log(currentHour);
+  function getLocal() {
+    var taskObj = JSON.parse(localStorage.getItem('tasks')) || {};
+
+    // For in loop iterates over each property of the taskObj
+    for (var inputId in taskObj) {
+      // If object has the property of the given inputId then add the value of that object to the input that matches that ID
+      if (taskObj.hasOwnProperty(inputId)) {
+        $("#" + inputId).val(taskObj[inputId]);
+      }
+    }
+  }
 
   function getHour(){
     for (var i = 9; i < 18; i++) {
+      // Setting current hour from string to int
+      currentHour = parseInt(currentHour);
       var hour = $("#hour-" + i); // This code will change what element we are talking about depending on what the value of i is
       // If the current hour is greater that i, remove future and display past 
       if (currentHour > i) {
@@ -37,7 +58,7 @@ $(function () {
       }
 
       // If the current hour is less than 9 or greater than 18, we will make add the future class to all timeslots
-      if (currentHour < 9 || currentHour > 18) {
+      if (currentHour < 9 || currentHour > 17) {
         hour.removeClass('past');
         hour.removeClass('present');
         hour.addClass('future');
@@ -45,12 +66,9 @@ $(function () {
     }
   }
 
-getHour();
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  $("#currentDay").text(today.format('dddd MMMM, D') + "th");
+  setInterval(getHour(), 1000);
+  getLocal();
+
+  // Added current day and time to the top of the page
+  $("#currentDay").text(today.format('dddd MMMM D') + "th, " + today.format("YYYY"));
 });
